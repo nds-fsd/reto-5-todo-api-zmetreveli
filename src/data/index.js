730c039ef8981fae9ce6be63f2a5b50e9226963c
zmetreveli <146349455+zmetreveli@users.noreply.github.com>
2023-11-12 18:49:41 +0100

    commit 1

--- src/data/index.js
+++ src/data/index.js
@@ -1,16 +1,14 @@
-exports.todos = [{
-  id: 0,
-  text: "Hacer la cama",
-  fecha: new Date("2022-05-21"),
-  done: false
-},
-{ 
-  id: 1,
-  text: "Ir al super",
-  fecha: new Date("2022-05-14"),
-  done: true
-}];
-
-
-
-
+exports.todos = [
+  {
+    id: 0,
+    text: "Hacer la cama",
+    fecha: new Date("2022-05-21"),
+    done: false,
+  },
+  {
+    id: 1,
+    text: "Ir al super",
+    fecha: new Date("2022-05-14"),
+    done: true,
+  },
+];
